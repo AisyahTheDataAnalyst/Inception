cf846d09fbab4d12cddf6811cf216ddc8a2d41a5
Aisyah Mokhtar <aimokhta@u80z06s02.42kl.edu.my>
2025-10-24 18:24:55 +0800

    fix: nginx.conf

--- src/requirements/nginx/tools/init_ssl.sh
+++ src/requirements/nginx/tools/init_ssl.sh
@@ -4,8 +4,8 @@ set -e
 mkdir -p /etc/nginx/ssl
 
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout /etc/nginx/ssl/inception.key \
-  -out /etc/nginx/ssl/inception.crt \
+  -keyout /etc/nginx/ssl/aimokhta.42.fr.key \
+  -out /etc/nginx/ssl/aimokhta.42.fr.crt \
   -subj "/C=MY/ST=KualaLumpur/L=KualaLumpur/O=42/OU=Student/CN=localhost"
 
 
