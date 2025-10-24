25dcd181d86b7faa69a11be3ffed9725a71bc159
Aisyah Mokhtar <aimokhta@u80z06s02.42kl.edu.my>
2025-10-24 08:57:32 +0800

    feat: added mariadb to test

---
+++ src/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+service mariadb start
+
+# Wait until the service is ready
+until mysqladmin ping >/dev/null 2>&1; do
+  sleep 1
+done
+
+# Create database and users
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin shutdown
+
+# Run MariaDB in foreground (PID 1)
+exec mysqld_safe
